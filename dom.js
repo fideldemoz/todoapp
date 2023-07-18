8fe700c67d4ce6907c632c8e080dd39a2154456b
Fidel Livino Pedro <fidellplivino98@gmail.com>
2023-07-18 10:07:04 +0200

    create dom listeners to open forms

--- dom.js
+++ dom.js
@@ -1,12 +1,17 @@
 const Projectform = document.querySelector("form.add-project"),
 Taskform = document.querySelector("form.add-task-form"),
-overlay = document.querySelector(".overlay"),
+overlay = document.querySelector(".o"),
 addProjectBtn = document.querySelector("button.plus");
 addTaskBtn = document.querySelector("button.add-task")
 
 addProjectBtn.addEventListener("click", () => {
 	Projectform.classList.toggle("show");
 })
-addTaskBtn.addEventListener("click", () => {
+addTaskBtn.addEventListener("click", (e) => {
+	overlay.classList.toggle('overlay');
 	Taskform.classList.toggle("show")
+})
+overlay.addEventListener("click", (e) => {
+	Taskform.classList.remove("show");
+	overlay.classList.remove('overlay')
 })
